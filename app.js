41fb9600c4b6448b512ae523234a0cc655b9b9d0
riyas <mriyastk8@gmail.com>
2025-08-09 11:58:33 +0530

    quary and params

---
+++ app.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import mongoose from 'mongoose';
+
+const app = express();
+
+app.use(express.json());
+
+//req.quary.....
+//http://localhost:5000/users?limit=5&page=7&name=riyas
+
+app.get('/', (req, res) => {
+  res.send('Server started');
+});
+
+app.get('/users', (req, res) => {
+  const { limit, page, name } = req.query;
+  res.send({ limit, page, name });  
+  console.log(req.query);           
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(5000, () => {
+  console.log('Server started on http://localhost:5000');
+});
